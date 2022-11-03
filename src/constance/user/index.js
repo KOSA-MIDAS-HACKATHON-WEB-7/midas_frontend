7862cfcd07fc291c57685dcc4740544b6cab81a9
kimkh05 <nestjs01@naver.com>
2022-11-03 20:36:33 +0900

    init dummydata

---
+++ src/constance/user/index.js
@@ -0,0 +1,9 @@
+export const User = new Array(12).fill({
+  name: "홍길동",
+  id: "qwer1234",
+  password: "pw1234@",
+  department: "midasIT",
+  position: "Frontend",
+  working: "Company",
+  coretime: ["09:00", "10:00"],
+});
